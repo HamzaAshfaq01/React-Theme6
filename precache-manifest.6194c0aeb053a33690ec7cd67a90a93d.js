616f6b31aac3c912d5e681b3ab2854a8c1cf9084
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2020-08-30 13:08:03 +0500

    Updates

---
+++ precache-manifest.6194c0aeb053a33690ec7cd67a90a93d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935886cee4dfec9ab72ece959b28eaf7",
+    "url": "/React-Theme6/index.html"
+  },
+  {
+    "revision": "23ff84b0ef540ae86fa0",
+    "url": "/React-Theme6/static/css/main.86916d09.chunk.css"
+  },
+  {
+    "revision": "c12f0827f6895a60ca7e",
+    "url": "/React-Theme6/static/js/2.ccc32840.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Theme6/static/js/2.ccc32840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ff84b0ef540ae86fa0",
+    "url": "/React-Theme6/static/js/main.07b7bf71.chunk.js"
+  },
+  {
+    "revision": "262ef23d3d14e446d721",
+    "url": "/React-Theme6/static/js/runtime-main.47b13366.js"
+  },
+  {
+    "revision": "95ebeec56b447db727a907ced484de7e",
+    "url": "/React-Theme6/static/media/1.95ebeec5.jpg"
+  },
+  {
+    "revision": "3edc0ff605c6d9680ae9505b5090a969",
+    "url": "/React-Theme6/static/media/icon-1.3edc0ff6.png"
+  },
+  {
+    "revision": "2667358fba8bcc1373ace00abdb0b284",
+    "url": "/React-Theme6/static/media/icon-5.2667358f.png"
+  },
+  {
+    "revision": "9fa71a0326ce101104600c21a3507fa2",
+    "url": "/React-Theme6/static/media/icon-6.9fa71a03.png"
+  },
+  {
+    "revision": "76f466c88efabe3385502a43c3576b0d",
+    "url": "/React-Theme6/static/media/img-7.76f466c8.png"
+  },
+  {
+    "revision": "c2f99b6fb1ddac7ca1b7bca34c14822f",
+    "url": "/React-Theme6/static/media/img-8.c2f99b6f.png"
+  },
+  {
+    "revision": "17c0d2c994aa09d827956bbe12abe982",
+    "url": "/React-Theme6/static/media/img-9.17c0d2c9.png"
+  },
+  {
+    "revision": "5f3e8636994198704b209e650413fc39",
+    "url": "/React-Theme6/static/media/img1.5f3e8636.png"
+  },
+  {
+    "revision": "3efe2c7bf15db0ce262e17fe5ee5aa21",
+    "url": "/React-Theme6/static/media/img2.3efe2c7b.png"
+  },
+  {
+    "revision": "35c9b188cb41e9c504ddc19c023141d9",
+    "url": "/React-Theme6/static/media/img3.35c9b188.png"
+  },
+  {
+    "revision": "86d7434bc357ef72d53c955a5845531b",
+    "url": "/React-Theme6/static/media/img4.86d7434b.png"
+  },
+  {
+    "revision": "89d5bd037eace179d23c0902b9c79019",
+    "url": "/React-Theme6/static/media/logo.89d5bd03.png"
+  },
+  {
+    "revision": "4802c11ebea7546a1771bbc3dfd4322e",
+    "url": "/React-Theme6/static/media/main.4802c11e.jpg"
+  }
+]);
